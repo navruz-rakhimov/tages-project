4c697934097668a2bc000c8841cd977a10efb992
Navruz Rakhimov <navruzbek.rakhimov@softensity.com>
2023-02-20 11:55:03 +0500

    use semphore instead of custom channels

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/google/uuid v1.3.0
+	golang.org/x/sync v0.1.0
 	google.golang.org/grpc v1.52.3
 	google.golang.org/protobuf v1.28.1
 )
--- server/services/image_service.go
+++ server/services/image_service.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 
+	"golang.org/x/sync/semaphore"
+
 	pb "github.com/navruz-rakhimov/tages-project/protos"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -18,113 +20,90 @@ const (
 
 type ImageServer struct {
 	pb.UnimplementedImageServiceServer
-	imageStore             ImageStore
-	readImageInfoSemaphore chan struct{}
-	uploadImageSempahore   chan struct{}
+	imageStore       ImageStore
+	readImageInfoSem *semaphore.Weighted
+	uploadImageSem   *semaphore.Weighted
 }
 
-func NewImageServer(imageStore ImageStore, maxReadConns int, maxUploadImageConns int) *ImageServer {
+func NewImageServer(imageStore ImageStore, maxReadConns int64, maxUploadImageConns int64) *ImageServer {
 	return &ImageServer{
-		imageStore:             imageStore,
-		readImageInfoSemaphore: make(chan struct{}, maxReadConns),
-		uploadImageSempahore:   make(chan struct{}, maxUploadImageConns),
+		imageStore:       imageStore,
+		readImageInfoSem: semaphore.NewWeighted(maxReadConns),
+		uploadImageSem:   semaphore.NewWeighted(maxUploadImageConns),
 	}
 }
 
 func (server *ImageServer) GetImageInfoList(ctx context.Context, _ *pb.Empty) (*pb.GetImageInfoListResponse, error) {
-	select {
-	case server.readImageInfoSemaphore <- struct{}{}:
-		defer func() {
-			<-server.readImageInfoSemaphore
-		}()
-
-		imageFullInfoList, err := server.imageStore.GetImagesInfoList()
-		if err != nil {
-			log.Fatal("Could get full image info list from image store: ", err)
-			return nil, err
-		}
-
-		return &pb.GetImageInfoListResponse{
-			ImageInfos: imageFullInfoList,
-		}, nil
-
-	default:
-		return nil, status.Errorf(codes.ResourceExhausted, "maximum number of connections for UnaryRPC reached")
+	if err := server.readImageInfoSem.Acquire(context.Background(), 1); err != nil {
+		return nil, err
 	}
+	defer func() {
+		server.readImageInfoSem.Release(1)
+	}()
 
+	imageFullInfoList, err := server.imageStore.GetImagesInfoList()
+	if err != nil {
+		log.Fatal("Could get full image info list from image store: ", err)
+		return nil, err
+	}
+	return &pb.GetImageInfoListResponse{
+		ImageInfos: imageFullInfoList,
+	}, nil
 }
 
 func (server *ImageServer) UploadImage(stream pb.ImageService_UploadImageServer) error {
-	select {
-	case server.uploadImageSempahore <- struct{}{}:
-		defer func() {
-			<-server.uploadImageSempahore
-		}()
-
-		req, err := stream.Recv()
-		if err != nil {
-			return logError(status.Errorf(codes.Unknown, "cannot receive image info"))
-		}
-
-		imageType := req.GetInfo().GetImageType()
-		imageName := req.GetInfo().GetImageName()
-		log.Printf("receive an upload-image request for image with type %s", imageType)
-
-		imageData := bytes.Buffer{}
-		imageSize := 0
-
-		for {
-			log.Print("waiting to receive more data")
-
-			req, err := stream.Recv()
-			if err == io.EOF {
-				log.Print("no more data")
-				break
-			}
-			if err != nil {
-				return logError(status.Errorf(codes.Unknown, "cannot receive chunk data: %v", err))
-			}
-
-			chunk := req.GetChunkData()
-			size := len(chunk)
-
-			log.Printf("received a chunk with size: %d", size)
-
-			imageSize += size
-			if imageSize > maxImageSize {
-				return logError(status.Errorf(codes.InvalidArgument, "image is too large: %d > %d", imageSize, maxImageSize))
-			}
-
-			// write slowly
-			// time.Sleep(time.Second)
-
-			_, err = imageData.Write(chunk)
-			if err != nil {
-				return logError(status.Errorf(codes.Internal, "cannot write chunk data: %v", err))
-			}
-		}
-
-		imageID, err := server.imageStore.Save(imageName, imageType, imageData)
-		if err != nil {
-			return logError(status.Errorf(codes.Internal, "cannot save image to the store: %v", err))
-		}
-
-		res := &pb.UploadImageResponse{
-			Id:   imageID,
-			Size: uint32(imageSize),
-		}
-
-		err = stream.SendAndClose(res)
-		if err != nil {
-			return logError(status.Errorf(codes.Unknown, "cannot send response: %v", err))
-		}
-
-		log.Printf("saved image with name: %s, size: %d", imageName, imageSize)
-		return nil
-
-	default:
-		return status.Errorf(codes.ResourceExhausted, "maximum number of connections for StreamingRPC reached")
+	if err := server.uploadImageSem.Acquire(context.Background(), 1); err != nil {
+		return err
 	}
+	defer func() {
+		server.uploadImageSem.Release(1)
+	}()
+
+	req, err := stream.Recv()
+	if err != nil {
+		return logError(status.Errorf(codes.Unknown, "cannot receive image info"))
+	}
+	imageType := req.GetInfo().GetImageType()
+	imageName := req.GetInfo().GetImageName()
+	log.Printf("receive an upload-image request for image with type %s", imageType)
+	imageData := bytes.Buffer{}
+	imageSize := 0
+	for {
+		log.Print("waiting to receive more data")
+		req, err := stream.Recv()
+		if err == io.EOF {
+			log.Print("no more data")
+			break
+		}
+		if err != nil {
+			return logError(status.Errorf(codes.Unknown, "cannot receive chunk data: %v", err))
+		}
+		chunk := req.GetChunkData()
+		size := len(chunk)
+		log.Printf("received a chunk with size: %d", size)
+		imageSize += size
+		if imageSize > maxImageSize {
+			return logError(status.Errorf(codes.InvalidArgument, "image is too large: %d > %d", imageSize, maxImageSize))
+		}
+		_, err = imageData.Write(chunk)
+		if err != nil {
+			return logError(status.Errorf(codes.Internal, "cannot write chunk data: %v", err))
+		}
+	}
+	imageID, err := server.imageStore.Save(imageName, imageType, imageData)
+	if err != nil {
+		return logError(status.Errorf(codes.Internal, "cannot save image to the store: %v", err))
+	}
+	res := &pb.UploadImageResponse{
+		Id:   imageID,
+		Size: uint32(imageSize),
+	}
+	err = stream.SendAndClose(res)
+	if err != nil {
+		return logError(status.Errorf(codes.Unknown, "cannot send response: %v", err))
+	}
+	log.Printf("saved image with name: %s, size: %d", imageName, imageSize)
+	return nil
 }
 
 func contextError(ctx context.Context) error {
